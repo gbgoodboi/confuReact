a0097e77930c22bd8dad3570b2192ad4b0977db6
Gi <baotdgde160069@fpt.edu.vn>
2022-10-04 22:01:17 +0700

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -3,18 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponents';
+import { DISHES } from './share/dishes';
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    }
+  }
   render() {
     return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu/>
-    </div>
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
     );
   }
 }
